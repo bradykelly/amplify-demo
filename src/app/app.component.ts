7f3da2b0a7b85c68630c3b25db7696d651d1390a
Brady Kelly <brady.kelly@lexisnexis.co.za>
2022-11-11 10:44:26 +0200

    Login and logout using hosted UI working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Auth } from "aws-amplify";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: "app-root",
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'amplify-demo';
+export class AppComponent implements OnInit {
+
+    title: string = "amplify-demo";
+    isUserLoggedIn: boolean = false;
+    userName: string = "";
+
+    ngOnInit(): void {
+        Auth.currentAuthenticatedUser()
+            .then(user => {
+                console.log(user);
+                this.isUserLoggedIn = true;
+                this.userName = user?.attributes?.name;
+            })
+            .catch(err => console.log(err));
+    }
+
+    login() {
+        Auth.federatedSignIn();
+    }
+
+    logout() {
+        Auth.signOut()
+            .then(data => {
+                console.log(data);
+                this.isUserLoggedIn = false;
+            })
+            .catch(err => console.log(err));
+    }
 }
+
+
